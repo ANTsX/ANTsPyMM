ada7afd8c647fbda10ffd033aa23547c3ea72b60
stnava <stnava@gmail.com>
2021-11-02 09:30:00 -0400

    ENH: test sr - at least dimensions

--- tests/testsr.py
+++ tests/testsr.py
@@ -1,9 +1,21 @@
+import sys, os
+import unittest
 import tensorflow as tf
 import ants
 import antspynet
 import antspymm
+
+import tempfile
+import shutil
+
 mdlfn = antspynet.get_pretrained_network( "mriSuperResolution" )
 mdl = tf.keras.models.load_model( mdlfn )
 img = ants.image_read( antspymm.get_data( "I1499279_Anon_20210819142214_5", target_extension=".nii.gz") )
-imgd = ants.resample_image( img, (16,16,16,16), use_voxels=True)
-srimg = antspymm.super_res_mcimage( imgd, mdl, verbose=True )
+lod = 16
+imgd = ants.resample_image( img, (lod,lod,lod,lod), use_voxels=True)
+srimg = antspymm.super_res_mcimage( imgd, mdl, verbose=False )
+
+testingClass = unittest.TestCase( )
+
+testingClass.assertEqual(
+    srimg.shape[0], lod*2, "dimension incorrect")
