7b2cd89af6742aa56d57ecf99734ffb093251c02
stnava <stnava@gmail.com>
2021-11-09 05:36:17 -0500

    BUG: forgot to declare function

--- antspymm/__init__.py
+++ antspymm/__init__.py
@@ -9,3 +9,4 @@ from .get_data import dewarp_imageset
 from .get_data import super_res_mcimage
 from .get_data import dipy_dti_recon
 from .get_data import segment_timeseries_by_meanvalue
+from .get_data import wmh
--- antspymm/get_data.py
+++ antspymm/get_data.py
@@ -1,6 +1,6 @@
 
 __all__ = ['get_data','dewarp_imageset','super_res_mcimage','dipy_dti_recon',
-    'segment_timeseries_by_meanvalue']
+    'segment_timeseries_by_meanvalue', 'wmh']
 
 from pathlib import Path
 from pathlib import PurePath
@@ -371,41 +371,39 @@ def dipy_dti_recon( image, bvalsfn, bvecsfn, median_radius = 4, numpass = 4, dil
         'RGB': ants.from_nibabel(nib.Nifti1Image(RGB.astype(np.float32), img.affine)) }
 
 def wmh( flair, t1, t1seg) :
-      
+
   """
   Outputs the WMH probability mask and a summary single measurement
-  
+
   Arguments
   ---------
   flair : ANTsImage
     input 3-D FLAIR brain image (not skull-stripped).
-  
+
   t1 : ANTsImage
     input 3-D T1 brain image (not skull-stripped).
-  
+
   t1seg : ANTsImage
     T1 segmentation image
 
- 
+
   Returns
   ---------
   WMH probability map and a summary single measurement which is the sum of the WMH map
-  
+
   """
-  
+
   probability_mask = antspynet.sysu_media_wmh_segmentation(flair)
   t1_2_flair_reg = ants.registration(flair, t1, type_of_transform = 'Rigid') # Register T1 to Flair
-  wmseg_mask = ants.get_mask(t1seg['segmentation_image'], low_thresh = 3, high_thresh = 3) # White matter label = 3
+  wmseg_mask = ants.threshold_image(
+    t1seg['segmentation_image'], low_thresh = 3, high_thresh = 3).iMath("FillHoles")
+    # White matter label = 3
   wmseg_2_flair = ants.apply_transforms(flair, wmseg_mask, transformlist = t1_2_flair_reg['fwdtransforms'])
-  probability_mask_WM = wmseg_2_flair * probability_mask # Remove WMH signal outside of WM 
+  probability_mask_WM = wmseg_2_flair * probability_mask # Remove WMH signal outside of WM
   label_stats = ants.label_stats(probability_mask_WM, wmseg_2_flair)
   label1 = label_stats[label_stats["LabelValue"]==1.0]
   wmh_sum = label1['Mass'].values[0]
 
   return{
-    'probability_mask' : probability_mask_WM,
-    'wmh_sum': wmh_sum }
-
-  return{
       'WMH_probability_map' : probability_mask_WM,
-      'wmh_sum': wmh_sum }
+      'wmh_mass': wmh_sum }
