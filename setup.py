97104acc2d00a62e7919cd0f471a72a1b272e8b4
stnava <stnava@gmail.com>
2021-11-16 09:13:49 -0500

    STYLE: v 7 prevent

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 long_description = open("README.md").read()
 
 setup(name='antspymm',
-      version='0.0.6',
+      version='0.0.7',
       description='multi-channel/time-series medical image processing with antspyx',
       long_description=long_description,
       long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
