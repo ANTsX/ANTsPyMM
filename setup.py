88b0665b6883682200c4e2739faca813f3a08fa7
stnava <stnava@gmail.com>
2022-12-20 10:37:59 -0500

    STYLE: bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("./requirements.txt") as f:
       requirements = f.read().splitlines()
 
 setup(name='antspymm',
-      version='0.6.1',
+      version='0.6.2',
       description='multi-channel/time-series medical image processing with antspyx',
       long_description=long_description,
       long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
