25e774de5692101a83143cc620b11d029cb5729e
LMFonville <leon.fonville@invicro.co.uk>
2023-01-06 14:59:15 +0000

    Update get_data.py
    added correlation matrix for all nodes found in powers_areal_mni_itk

--- antspymm/get_data.py
+++ antspymm/get_data.py
@@ -2338,13 +2338,47 @@ def resting_state_fmri_networks( fmri, t1, t1segmentation,
   gmmat = ants.regress_components( gmmat, nuisance )
 
   myfalff=alff_image( simg, bmask, flo=f[0], fhi=f[1] ) #  nuisance=nuisance )
-
+  
+  # add correlation matrix that captures each node pair
+  # get correlations for all ROIs
+  # could also set this to just the selected networks if need be?
+  nPoints=pts2bold['ROI'].max()
+    
+    
   networks = powers_areal_mni_itk['SystemName'].unique()
 
   outdict = {}
   outdict['meanBold'] = und
   outdict['pts2bold'] = pts2bold
+  # preallocate correlation matrix and use simple for loops to fill
+  roiMat = np.zeros([nPoints, nPoints] )
+  roiNames = []
+  for i in range(nPoints):
+    # specify name for matrix entries that's easy to link back; e.g., ROI1_InferiorOccipitalGyrus
+    roiLabel="ROI" + str(powers_areal_mni_itk.loc[i,'ROI']) + '_' + re.sub( " ", "", powers_areal_mni_itk.loc[i,'Anatomy'] )
+    roiNames.append( roiLabel )
+    for j in range(nPoints):
+        # skip diagonals in this case
+        if i==j:
+            roiMat[i,j] = 1.0
+        else:
+            # find indices of ROI in df
+            idxRoi1=np.where( powers_areal_mni_itk['ROI'] == i+1 )[0]
+            idxRoi2=np.where( powers_areal_mni_itk['ROI'] == j+1 )[0]
+            # use indices to make a sphere at x,y,z coordinates and take average within that sphere
+            roi1 = ants.timeseries_to_matrix( simg, ants.make_points_image(pts2bold.iloc[idxRoi1,:3].values, bmask, radius=1).threshold_image( 1, 1e9 )).mean(axis=1)
+            roi2 = ants.timeseries_to_matrix( simg, ants.make_points_image(pts2bold.iloc[idxRoi2,:3].values, bmask, radius=1).threshold_image( 1, 1e9 )).mean(axis=1)
+            # calculate simple correlation between the two timeseries
+            roiMat[i,j] = pearsonr(roi1, roi2)[0]
 
+  # store output as dataframe
+  outputMat = pd.DataFrame(roiMat)
+  outputMat.columns = roiNames
+  outputMat['ROIs']=roiNames
+  # add to dictionary and done?
+  outdict['fullCorrMat'] = outputMat
+    
+    
   # this is just for human readability - reminds us of which we choose by default
   netnames = ['Cingulo-opercular Task Control', 'Default Mode',
                 'Memory Retrieval', 'Ventral Attention', 'Visual',
