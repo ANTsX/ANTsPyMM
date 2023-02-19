479793c2a3167619535c90ac60898622b6b19bb0
stnava <stnava@gmail.com>
2023-02-18 20:24:54 -0500

    BUG: at last - again - wrong reference space

--- antspymm/get_data.py
+++ antspymm/get_data.py
@@ -198,7 +198,7 @@ def dti_reg(
         print("Progress:")
     counter = round( nTimePoints / 10 )
     for k in range(nTimePoints):
-        if verbose and ( ( k % counter) ==  0 ) or ( k == (nTimePoints-1) ) ):
+        if verbose and ( ( k % counter ) ==  0 ) or ( k == (nTimePoints-1) ):
             myperc = round( k / nTimePoints * 100)
             print(myperc, end="%.", flush=True)
         if k in b0_idx:
