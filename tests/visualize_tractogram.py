8bae746bfac17cee9f739641f6242a710eaeafcd
stnava <stnava@gmail.com>
2023-02-28 19:56:50 -0500

    WIP: clean up  ...

--- tests/visualize_tractogram.py
+++ tests/visualize_tractogram.py
@@ -12,22 +12,23 @@ from dipy.tracking.utils import density_map
 from dipy.data.fetcher import (fetch_file_formats,
                                get_file_formats)
 
-ref_anat_filename = 'processed_or/data/PPMI/100018/20210202/DTI_LR/1497589/PPMI-100018-20210202-DTI_LR-1497589-I1497578-DTIRGB.nii.gz'
-trkfn = 'processed_or/data/PPMI/100018/20210202/DTI_LR/1497589/PPMI-100018-20210202-DTI_LR-1497589-I1497578-tractogram.trk'
-reference_anatomy = nib.load(ref_anat_filename)
+if False:
+    ref_anat_filename = 'processed_or/data/PPMI/100018/20210202/DTI_LR/1497589/PPMI-100018-20210202-DTI_LR-1497589-I1497578-DTIRGB.nii.gz'
+    trkfn = 'processed_or/data/PPMI/100018/20210202/DTI_LR/1497589/PPMI-100018-20210202-DTI_LR-1497589-I1497578-tractogram.trk'
+    reference_anatomy = nib.load(ref_anat_filename)
 
-if not 'trk' in locals():
-    trk = load_tractogram(trkfn, reference_anatomy)
+    if not 'trk' in locals():
+        trk = load_tractogram(trkfn, reference_anatomy)
 
-streamlines = select_random_set_of_streamlines(trk.streamlines, 10000)
+    streamlines = select_random_set_of_streamlines(trk.streamlines, 10000)
 
-from fury import actor, colormap
-from dipy.tracking import utils
-# from utils.visualization_utils import generate_anatomical_volume_figure
-streamlines_actor = actor.line(streamlines, colormap.line_colors(streamlines))
+    from fury import actor, colormap
+    from dipy.tracking import utils
+    # from utils.visualization_utils import generate_anatomical_volume_figure
+    streamlines_actor = actor.line(streamlines, colormap.line_colors(streamlines))
 
-from dipy.viz import window, actor, colormap, has_fury
-scene = window.Scene()
-scene.add(actor.line(streamlines,
-        colormap.line_colors(streamlines)))
-window.show(scene, size=[2000,2000])
+    from dipy.viz import window, actor, colormap, has_fury
+    scene = window.Scene()
+    scene.add(actor.line(streamlines,
+            colormap.line_colors(streamlines)))
+    window.show(scene, size=[2000,2000])
