aa689b623152919c1a17208969529fa7148a70d2
stnava <stnava@gmail.com>
2023-05-09 13:48:26 -0400

    ENH: save origin in blind qc

--- antspymm/mm.py
+++ antspymm/mm.py
@@ -6736,6 +6736,7 @@ def blind_image_assessment(
 #        obj = BRISQUE(url=False)
 #        mybrisq = obj.score( np.array( Image.open( viz_filename )) )
         spc = ants.get_spacing( image )
+        org = ants.get_origin( image )
         msk_vol = msk.sum() * np.prod( spc )
         bgstd = image[ bgmsk == 1 ].std()
         fgmean = image[ msk == 1 ].mean()
@@ -6764,7 +6765,7 @@ def blind_image_assessment(
         if viz_filename is not None and ( jjj == 0 or (jjj % 30 == 0) ):
             viz_filename_use = re.sub( ".png", "_slice"+str(jjj).zfill(4)+".png", viz_filename )
             ants.plot_ortho( image, crop=False, filename=viz_filename_use, flat=True, xyz_lines=False, orient_labels=False, xyz_pad=0,  title=ttl, titlefontsize=12, title_dy=-0.02,textfontcolor='red' )
-        df = pd.DataFrame([[ mystem, noizlevel, snrref, cnrref, psnrref, ssimref, mymi, asym_err, myevr, msk_vol, spc[0], spc[1], spc[2], image.shape[0], image.shape[1], image.shape[2], jjj, modality, mriseries, mrimfg, mrimodel ]], columns=['fn', 'noise', 'snr', 'cnr', 'psnr', 'ssim', 'mi', 'reflection_err', 'EVR', 'msk_vol', 'spc0','spc1','spc2','dimx','dimy','dimz','slice','modality', 'mriseries', 'mrimfg', 'mrimodel' ])
+        df = pd.DataFrame([[ mystem, noizlevel, snrref, cnrref, psnrref, ssimref, mymi, asym_err, myevr, msk_vol, spc[0], spc[1], spc[2],org[0], org[1], org[2], image.shape[0], image.shape[1], image.shape[2], jjj, modality, mriseries, mrimfg, mrimodel ]], columns=['fn', 'noise', 'snr', 'cnr', 'psnr', 'ssim', 'mi', 'reflection_err', 'EVR', 'msk_vol', 'spc0','spc1','spc2','org0','org1','org2','dimx','dimy','dimz','slice','modality', 'mriseries', 'mrimfg', 'mrimodel' ])
         outdf = pd.concat( [outdf, df ], axis=0 )
         if verbose:
             print( outdf )
