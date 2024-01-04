b6d68000cb961163cd51ee4464b86279f90828f5
stnava <stnava@gmail.com>
2024-01-04 11:19:28 -0500

    WIP: update defaults for perfusion

--- tests/test_perfusion_ptbp.py
+++ tests/test_perfusion_ptbp.py
@@ -12,8 +12,6 @@ import ants
 import numpy as np
 from scipy.stats import median_abs_deviation
 import math
-t1fn = "./PEDS049/20110217/T1w/000/PTBP-PEDS049-20110217-T1w-000.nii.gz"
-idpfn = "./PEDS049/20110217/perf/000/PTBP-PEDS049-20110217-perf-000.nii.gz"
 t1fn = "/tmp/data/PEDS015/20110812/Anatomy/PEDS015_20110812_mprage_t1.nii.gz"
 idpfn = "/tmp/data/PEDS015/20110812/PCASL/PEDS015_20110812_pcasl_1.nii.gz"
 t1fn = '/tmp/data/PEDS022/20111216/Anatomy/PEDS022_20111216_mprage_t1.nii.gz'
@@ -22,6 +20,8 @@ t1fn = '/tmp/data/PEDS041/20110127/Anatomy/PEDS041_20110127_mprage_t1.nii.gz'
 idpfn = '/tmp/data/PEDS041/20110127/PCASL/PEDS041_20110127_pcasl_1.nii.gz'
 t1fn='/tmp/data/PEDS014/20100831/Anatomy/PEDS014_20100831_mprage_t1.nii.gz'
 idpfn='/tmp/data/PEDS014/20100831/PCASL/PEDS014_20100831_pcasl_1.nii.gz'
+t1fn = "/Users/stnava/data/PTBP/PTBP/PEDS049/20110217/T1w/000/PTBP-PEDS049-20110217-T1w-000.nii.gz"
+idpfn = "/Users/stnava/data/PTBP/PTBP/PEDS049/20110217/perf/000/PTBP-PEDS049-20110217-perf-000.nii.gz"
 if not 'dkt' in globals():
   t1head = ants.image_read( t1fn ).n3_bias_field_correction( 8 ).n3_bias_field_correction( 4 )
   t1bxt = antspynet.brain_extraction( t1head, 't1' ).threshold_image( 0.3, 1.0 )
@@ -44,6 +44,7 @@ perf = antspymm.bold_perfusion( fmri, fmri_template, t1head, t1,
 ants.image_write( ants.iMath( perf['perfusion'], "Normalize" ), '/tmp/temp.nii.gz' )
 ants.image_write( perf['motion_corrected'], '/tmp/temp2.nii.gz' )
 ants.plot( ants.iMath( perf['perfusion'], "Normalize" ), axis=2, crop=True )
+ants.plot( ants.iMath( perf['cbf'], "Normalize" ), axis=2, crop=True )
 
 if False:
     print("do perf2")
