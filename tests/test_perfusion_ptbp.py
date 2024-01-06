87189d6760302b368810158e32186474944c7c0d
stnava <stnava@gmail.com>
2024-01-06 12:43:29 -0500

    PERF: outlier threshold

--- tests/test_perfusion_ptbp.py
+++ tests/test_perfusion_ptbp.py
@@ -40,7 +40,7 @@ fmri_template = ants.get_average_of_timeseries( fmri_template )
 print("do perf")
 # olthresh=0.5
 perf = antspymm.bold_perfusion( fmri, fmri_template, t1head, t1, 
-  t1segmentation, dkt, trim_the_mask=3.0, verbose=True )
+  t1segmentation, dkt, trim_the_mask=3.0, outlier_threshold=0.5, verbose=True )
 ants.image_write( ants.iMath( perf['perfusion'], "Normalize" ), '/tmp/temp.nii.gz' )
 ants.image_write( perf['motion_corrected'], '/tmp/temp2.nii.gz' )
 ants.image_write( perf['cbf'], '/tmp/temp3ptb.nii.gz' )
