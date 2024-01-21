0ce9087216a3e476b560033bbf3c7124d347a773
stnava <stnava@gmail.com>
2024-01-21 18:32:33 -0500

    STYLE: update minimal rsfmri test

--- tests/test_rsfmri_run_minimal.py
+++ tests/test_rsfmri_run_minimal.py
@@ -1,10 +1,11 @@
+################################################################
 import sys, os
 import unittest
-
+################################################################
 os.environ["TF_NUM_INTEROP_THREADS"] = "8"
 os.environ["TF_NUM_INTRAOP_THREADS"] = "8"
 os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = "8"
-
+################################################################
 import tempfile
 import shutil
 import tensorflow as tf
@@ -13,15 +14,15 @@ import antspyt1w
 import antspynet
 import ants
 import numpy as np
-
+################################################################
 testingClass = unittest.TestCase( )
 islocal = False
 id1 = "LS2001_3T_rfMRI_REST1_LR_gdc"
 img1 = ants.image_read( antspymm.get_data( id1, target_extension=".nii.gz") )
-
+################################################################
 import pandas as pd
 und = ants.get_average_of_timeseries( img1 )
-
+################################################################
 t1fn = antspymm.get_data( 'LS2001_3T_T1w_MPR1_gdc' , target_extension='.nii.gz' )
 print("do t1")
 if not "t1" in globals():
@@ -29,24 +30,19 @@ if not "t1" in globals():
     t1bxt = antspynet.brain_extraction( t1, 't1' ).threshold_image( 0.3, 1.0 )
     t1seg = antspynet.deep_atropos( t1 )
     t1segmentation = t1seg['segmentation_image']
-
+################################################################
 print("do rsf")
 rsf = antspymm.resting_state_fmri_networks(
   img1, und, t1 * t1bxt, t1segmentation, 
-  impute=False,
-  scrub=False,
+  nc=5,
+  censor=False,
   verbose=True )
+################################################################
 rsfscrub = antspymm.resting_state_fmri_networks(
   img1, und, t1 * t1bxt, t1segmentation, 
-  impute=False,
+  nc=0.5,
   scrub=True,
-  verbose=True )
-rsfimp = antspymm.resting_state_fmri_networks(
-  img1, und, t1 * t1bxt, t1segmentation, 
-  impute=True,
-  scrub=False,
   verbose=True )
 ants.plot( und, rsf['DefaultMode'], crop=True, axis=2 )
 ants.plot( und, rsfscrub['DefaultMode'], crop=True, axis=2 )
-ants.plot( und, rsfimp['DefaultMode'], crop=True, axis=2 )
-
+################################################################
