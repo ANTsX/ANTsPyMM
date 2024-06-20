8a4ba378d774a355634738d70a916249c1c2ec9e
stnava <stnava@gmail.com>
2024-06-20 08:29:38 -0400

    WIP: ortho stack DFN

--- antspymm/mm.py
+++ antspymm/mm.py
@@ -9581,11 +9581,11 @@ def quick_viz_mm_nrg(
             mod_search_path = os.path.join(subjectrootpath, 'rsfMRI*', "*", "*nii.gz")
             if post:
                 mod_search_path = os.path.join(subjectrootpath, 'rsfMRI*', "*", "*fcnxpro122_meanBold.nii.gz" )
-                mod_search_path_ol = os.path.join(subjectrootpath, 'rsfMRI*', "*", "*fcnxpro122_DefaultMode.nii.gz.nii.gz" )
+                mod_search_path_ol = os.path.join(subjectrootpath, 'rsfMRI*', "*", "*fcnxpro122_DefaultMode.nii.gz" )
                 myol = glob.glob(mod_search_path_ol)
                 if len( myol ) > 0:
                     underlay = ants.image_read( myol[0] )
-                    underlay = underlay * ants.threshold_image( underlay, 0.1, 1.0 )
+                    underlay = underlay * ants.threshold_image( underlay, 0.1, math.inf )
             myimgsr = glob.glob(mod_search_path)
             if len( myimgsr ) > 0:
                 myimgsr.sort()
