a00afd4ed43424e418bfa5682509efa37b241bf3
stnava <stnava@gmail.com>
2024-07-02 18:50:05 -0400

    BUG: missed one region for pym plot

--- antspymm/mm.py
+++ antspymm/mm.py
@@ -10767,6 +10767,11 @@ def brainmap_figure(statistical_df, data_dictionary_path, output_prefix, brain_i
             anatsear=re.sub(".jhu.icbm.labels.1mm","",anatsear)
             anatsear=re.sub("mtg.sn.snc.",".snc.",anatsear)
             anatsear=re.sub("mtg.sn.snr.",".snr.",anatsear)
+            anatsear=re.sub("anterior.","ant.",anatsear)
+            anatsear=re.sub("ant.corona.radiata","ant.cor.rad",anatsear)
+            anatsear=re.sub("sup.corona.radiata","sup.cor.rad",anatsear)
+            anatsear=re.sub("posterior.thalamic.radiation.include.optic.radiation","post.thalamic.radiation",anatsear)
+            anatsear=re.sub("retrolenticular.part.of.internal.capsule","rent.int.cap",anatsear)
             atlassearch = mydict['tidynames'].str.contains(anatsear)
             if atlassearch.sum() == 0:
                 atlassearch = mydict2['tidynames'].str.contains(anatsear)
